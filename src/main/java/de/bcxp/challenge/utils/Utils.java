04e7e852deaf5971952c3b095cc07f69c8d1cbdf
chdacruz <47527129+chdacruz@users.noreply.github.com>
2025-06-01 16:10:32 +0100

    Improvements to readCS(), exception mapping

--- src/main/java/de/bcxp/challenge/utils/Utils.java
+++ src/main/java/de/bcxp/challenge/utils/Utils.java
@@ -1,38 +1,27 @@
 package de.bcxp.challenge.utils;
 
+import de.bcxp.challenge.exceptions.ChallengeException;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Utils {
+
     public static List<List<String>> readCSV(String fileName) {
-        List<List<String>> csvData = new ArrayList<>();
-
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                // Split on comma but not within quotes
-                String[] values = line.split(",");
-
-                // Convert array to list while removing quotes and trimming whitespace
-                List<String> row = new ArrayList<>();
-                for (String value : values) {
-                    value = value.trim();
-                    if (value.startsWith("\"") && value.endsWith("\"")) {
-                        value = value.substring(1, value.length() - 1);
-                    }
-                    row.add(value);
-                }
-                csvData.add(row);
-            }
+            return br.lines()
+                    .map(line -> line.split(","))
+                    .map(values -> Arrays.stream(values)
+                            .map(String::trim)
+                            .collect(Collectors.toList()))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
-            //TODO Check if it's needed to improve this
-            e.printStackTrace();
+            throw new ChallengeException.CSVReadException("Error reading CSV file", e);
         }
-
-        return csvData;
     }
 }
 
